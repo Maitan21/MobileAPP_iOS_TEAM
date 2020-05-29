9c9153c991c822a4d8490bb3db4d3483ea927389
YoonSang2 <oklfg2002@gmail.com>
2020-05-29 19:29:22 +0900

    Jho_Webview and Camera,album

--- knuunse/knuunse/FaceUnseViewController.swift
+++ knuunse/knuunse/FaceUnseViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import WebKit
 
-class FaceUnseViewController : UIViewController{
+class FaceUnseViewController : UIViewController, WKNavigationDelegate{
+    @IBOutlet weak var WebView: WKWebView!
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      
@@ -18,12 +23,38 @@ class FaceUnseViewController : UIViewController{
         button.setTitleColor(.link, for: .normal)
         button.addTarget(self,action: #selector(clickBack), for: .touchUpInside)
         button.sizeToFit()
+        
 
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+        
+            // Do any additional setup after loading the view.
+             WebView.load(URLRequest(url: URL(string: "https://yoonsang2.github.io/MobileAPP_iOS_TEAM/")!))
+                    
+            self.WebView.navigationDelegate = self
+            //        refresh
+            let refreshControl = UIRefreshControl()
+            refreshControl.addTarget(self, action: #selector(refreshWebView(_:)), for: UIControl.Event.valueChanged)
+            WebView.scrollView.addSubview(refreshControl)
+            WebView.scrollView.bounces = true
+
+
     }
    
+    
+    @objc func refreshWebView(_ sender: UIRefreshControl){
+               WebView?.reload()
+               sender.endRefreshing()
+           }
+    
    @objc func clickBack(_ sender:Any)
    {
        tabBarController?.selectedIndex = 0
    }
+    
+    func webView(_ WebView2: WKWebView, didFinish navigation: WKNavigation!) {
+           // hide/stop indicator
+           
+           indicator.isHidden = true
+           indicator.stopAnimating()
+       }
 }
