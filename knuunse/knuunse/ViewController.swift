1f43a1abe0affb83475cc449d4e3d06c099321f1
jinminsin <45217459+jinminsin@users.noreply.github.com>
2020-05-29 18:13:39 +0900

    ui initialize ver2
    설정 추가

--- knuunse/knuunse/ViewController.swift
+++ knuunse/knuunse/ViewController.swift
@@ -9,22 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var stackView: [UIStackView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        for i in stackView{
+        i.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(clickButton(_:))))
+        }
+        
     }
 
-    @IBAction func clickButton(_ sender: Any) {
-        if (sender as? UIButton)?.currentTitle == "별자리 운세"
+    @objc func clickButton(_ sender: Any) {
+        let id = (sender as? UITapGestureRecognizer)?.view?.restorationIdentifier
+        if id == "별자리 운세"
         {
             tabBarController?.selectedIndex = 1
-        }else if (sender as? UIButton)?.currentTitle == "오늘의 운세"
+        }else if id == "오늘의 운세"
         {
             tabBarController?.selectedIndex = 2
-        }else{
+        }else if id == "관상" {
             tabBarController?.selectedIndex = 3
+        }else{
+            tabBarController?.selectedIndex = 4
         }
     }  
 }
